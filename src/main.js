b870cca2186d61d469374a616d15a94afea9bc5f
web <admin>
2019-09-19 18:05:49 +0800

    登录完成

--- src/main.js
+++ src/main.js
@@ -2,7 +2,40 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+import {
+  Button,
+  Cell,
+  CellGroup,
+  NavBar,
+  Field,
+  Toast
+} from 'vant'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 加载验证插件的语言包
+import * as rules from 'vee-validate/dist/rules'
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+extend('phone', {
+  validate (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  message: '必须是手机号'
+})
+Vue
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)
+  .use(NavBar)
+  .use(Field)
+  .use(Toast)
 
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="login">
+    <van-nav-bar title="登录" />
+    <van-cell-group>
+      <ValidationObserver tag="form" ref="loginForm">
+        <ValidationProvider tag="div" name="手机号" rules="required|phone" v-slot="{ errors }">
+          <van-field
+            v-model="user.mobile"
+            required
+            clearable
+            label="手机"
+            placeholder="请输入手机号"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+        <ValidationProvider tag="div" name="验证码" rules="required" v-slot="{ errors }">
+          <van-field
+            v-model="user.code"
+            type="password"
+            label="验证码"
+            placeholder="请输入验证码"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+      </ValidationObserver>
+    </van-cell-group>
+    <div class="login-btn">
+      <van-button type="info" :loading="isLoginLoading" @click="onLogin">登录</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { login } from '@/api/user'
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      },
+      isLoginLoading: false
+    }
+  },
+  methods: {
+    async onLogin () {
+      try {
+        const isValid = await this.$refs.loginForm.validate()
+        if (!isValid) {
+          return
+        }
+        this.isLoginLoading = true
+        const { data } = await login(this.user)
+        console.log(data)
+        this.$toast.success('登录成功')
+      } catch (err) {
+        if (err.response && err.response.status === 400) {
+          this.$toast.fail('手机号或验证码错误')
+        }
+      }
+      this.isLoginLoading = false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-btn {
+  padding: 20px;
+  .van-button {
+    width: 100%;
+  }
+}
+</style>
